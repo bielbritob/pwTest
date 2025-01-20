36007132b3403ca02948573508064697c39b035e
bielbritob <123404262+bielbritob@users.noreply.github.com>
2025-01-20 01:00:35 +0000

    attempt to push....

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# 🎈 Blank app template
+# 🛒💰 PwTest
 
-A simple Streamlit app template for you to modify!
+A simple Streamlit app test
 
 [![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://blank-app-template.streamlit.app/)
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,17 @@ if "first_run" not in st.session_state:
     st.session_state.first_run = True
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+inputext = st.text_input('Digite o produto:', placeholder='Ex. Leite integral...')
+
+if st.button('Pesquisar'):
+    with st.spinner('Pesquisando...'):
+        # Define o argumento com base na primeira execução
+        select_city = st.session_state.first_run
+        subOS = subprocess.run(["python", "scrapData.py", inputext, str(select_city)])
+
+        # Atualiza o estado para False após a primeira execução
+        if st.session_state.first_run:
+            st.session_state.first_run = False
+
+        st.write(subOS)
+#
